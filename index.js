7b11db34911c9a5bbeecb12a6574798b0ca65373
Jacqueline712 <mondaystudio712@gmail.com>
2023-05-31 15:20:35 +0800

    add:①生成文章功能;②将文章输出;③用fs保存文件;④增加文件时间戳

--- index.js
+++ index.js
@@ -4,23 +4,25 @@ import {generate} from './lib/generator.js';
 import {createRandomPicker} from './lib/random.js';
 import {interact} from './lib/interact.js';
 
+// 将读取JSON文件的代码封装成一个函数loadCorpus
 const corpus = loadCorpus('corpus/data.json');
+// 通过pickTitle随机选择一个title
 let title = options.title || createRandomPicker(corpus.title)();
 
 (async function () {
-  if(Object.keys(options).length <= 0) {
-    const answers = await interact([
-      {text: '请输入文章主题', value: title},
-      {text: '请输入最小字数', value: 6000},
-      {text: '请输入最大字数', value: 10000},
-    ]);
-    title = answers[0];
-    options.min = answers[1];
-    options.max = answers[2];
-  }
+    if (Object.keys(options).length <= 0) {
+        const answers = await interact([
+            {text: '请输入文章主题', value: title},
+            {text: '请输入最小字数', value: 6000},
+            {text: '请输入最大字数', value: 10000}
+        ]);
+        title = answers[0];
+        options.min = answers[1];
+        options.max = answers[2];
+    }
+    // 调用generate函数，拿到article数组
+    const article = generate(title, {corpus, ...options});
+    const output = saveCorpus(title, article);
 
-  const article = generate(title, {corpus, ...options});
-  const output = saveCorpus(title, article);
-
-  console.log(`生成成功！文章保存于：${output}`);
-}());
+    console.log(`生成成功！文章保存于：${output}`);
+})();
