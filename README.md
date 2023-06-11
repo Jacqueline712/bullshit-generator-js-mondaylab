2ad0146480b2b58d55c7c6478d4d5ffecbcc6e4f
Jacqueline712 <mondaystudio712@gmail.com>
2023-06-11 15:21:23 +0800

    add:文章增加命令行交互

--- README.md
+++ README.md
@@ -21,11 +21,14 @@
 ```
 .
 ├── corpus 存放语料库文件
-│   └── data.json
+	  ├── data.json
 ├── index.js 项目主文件，是一个可以运行的node.js脚本
 ├── lib 项目依赖的库文件
-│   ├── generator.js 用来生成文章内容
-│   └── random.js 用来提供随机算法
+	  ├── cmd.js 实现带参数的命令行交互
+   	  ├── generator.js 用来生成文章内容
+       ├── random.js 用来提供随机算法
+	  ├── corpus.js 语料库，用来加载和保存文件功能（即loadCorpus和saveCorpus）
+	  ├── interact,ts 文章生成器的交互过程
 ├── package.json 配置文件
 └── output 存放项目输入结果
 
@@ -38,6 +41,30 @@ data.json：
 - famous —— 名人名言
 - bosh —— 废话
 - bosh_before/said/conclude —— 用来修饰和替换famous以及bosh里面的内容
+
+
+
+两种读取命令行参数的方式：
+
+- `process.argv`
+- `command-line-args`
+
+
+
+两种命令行交互的方式：
+
+- `process.stdin` —— `process.stdin`是异步的，它继承`EventEmitter`对象，能够派发和监听事件。
+- `readline` —— node.js为我们提供的一个更好的内置模块，它是专门用来实现可交互命令行的。
+- `process.stdout.write` —— 可以向终端输出字符，可以理解为和`cosole.log`一样的效果。区别是 `console.log` 支持多个参数，且能够格式化字符串并自动输出回车符。也就是说，`console.log`基本上可以替代`process.stdout.write`功能且更强大，所以我们就基本上不需要使用`process.stdout.write`了。不过呢，如果我们不希望输出回车换行到终端，那还是可以使用`process.stdout.write`的。
+- ``
+
+
+
+
+
+
+
+
 
 
 
--- index.js
+++ index.js
@@ -1,28 +1,38 @@
-import {options} from './lib/cmd.js';
-import {loadCorpus, saveCorpus} from './lib/corpus.js';
-import {generate} from './lib/generator.js';
-import {createRandomPicker} from './lib/random.js';
-import {interact} from './lib/interact.js';
+import { options } from './lib/cmd.js';
+import { loadCorpus, saveCorpus } from './lib/corpus.js';
+import { generate } from './lib/generator.js';
+import { createRandomPicker } from './lib/random.js';
+import { interact } from './lib/interact.js';
 
 // 将读取JSON文件的代码封装成一个函数loadCorpus
 const corpus = loadCorpus('corpus/data.json');
 // 通过pickTitle随机选择一个title
 let title = options.title || createRandomPicker(corpus.title)();
 
+// 通过async/await的方式，等待用户回答所有问题后，再进行文章生成的操作
 (async function () {
-    if (Object.keys(options).length <= 0) {
-        const answers = await interact([
-            {text: '请输入文章主题', value: title},
-            {text: '请输入最小字数', value: 6000},
-            {text: '请输入最大字数', value: 10000}
-        ]);
-        title = answers[0];
-        options.min = answers[1];
-        options.max = answers[2];
-    }
-    // 调用generate函数，拿到article数组
-    const article = generate(title, {corpus, ...options});
-    const output = saveCorpus(title, article);
+  if (Object.keys(options).length <= 0) {
+    const answers = await interact(
+      /**
+       * 设计一个问题的数据结构
+       * 包含3个问题对象：
+       * text —— 问题的文字描述
+       * value —— 表示问题的默认值
+       */
+      [
+        { text: '请输入文章主题', value: title },
+        { text: '请输入最小字数', value: 6000 },
+        { text: '请输入最大字数', value: 10000 }
+      ]
+    );
+    title = answers[0];
+    options.min = answers[1];
+    options.max = answers[2];
+  }
+  // 调用generate函数，拿到article数组
+  // options是相对应的配置项
+  const article = generate(title, { corpus, ...options });
+  const output = saveCorpus(title, article);
 
-    console.log(`生成成功！文章保存于：${output}`);
+  console.log(`生成成功！文章保存于：${output}`);
 })();
